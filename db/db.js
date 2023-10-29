3218735d7e4dd8fb92986d455ef146bbb88bacfa
faisalbaqirt <faishalb0@gmail.com>
2023-10-29 16:42:05 +0700

    fix errorrrr

--- db/db.js
+++ db/db.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const knexfile = require('../knexfile');
 
-const db = knex(knexfile.production);
+// Ubah lingkungan yang digunakan berdasarkan nilai NODE_ENV
+const environment = process.env.NODE_ENV || 'development';
+const db = knex(knexfile[environment]);
 
 module.exports = db;
